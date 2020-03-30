f3eebee0cdb00b3232cbb3cbed4e45edac3608dc
smajum dar22 <smajumdar22@gmail.com>
2020-03-29 20:41:38 -0700

    Refactored

--- __tests__/validator.test.js
+++ __tests__/validator.test.js
@@ -87,33 +87,4 @@ describe('validator module performs basic validation of', () => {
     }
   });
 
-
-
-describe('validator module performs complex validations', () => {
-
-  it('validates the presence of required object properties at any level', () => {
-    // i.e. does person.hair.color exist and have a good value, not just person.hair
-    expect(true).toBeFalsy();
-  });
-
-  it('validates the proper types of object properties', () => {
-    // i.e. person.name must be a string, etc.
-    expect(true).toBeFalsy();
-  });
-
-  it('validates the types of values contained in an array', () => {
-    // i.e. an array of all strings or numbers
-    expect(true).toBeFalsy();
-  });
-
-  it('validates a value array against an approved list', () => {
-    // i.e. a string might only be allowed to be "yes" or "no"
-    expect(true).toBeFalsy();
-  });
-
 });
-
-});
-
-
-
